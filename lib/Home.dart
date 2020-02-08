c63b5168709b5f75842a1508302f7bf123746375
MananS10 <55032652+MananS10@users.noreply.github.com>
2020-02-08 17:27:57 +0530

    Added Login button and functionality.

--- lib/Home.dart
+++ lib/Home.dart
@@ -8,6 +8,9 @@ import 'package:junkjar/Colours.dart';
 import 'package:junkjar/GoogleButton.dart';
 import 'package:junkjar/TestPage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:junkjar/sign_in.dart';
+
+import 'FirstScreen.dart';
 
 class MyHomePage extends StatefulWidget {
   @override
@@ -30,25 +33,62 @@ class _MyHomePageState extends State<MyHomePage> {
               SizedBox(
                 height: 75.0,
               ),
-              Container(height: 175.0, width: 175.0,
+              Container(
+                height: 175.0, width: 175.0,
 //                child: Image.asset('assets/plastic.png'),
-                  child: FlareActor("assets/recycle.flr", alignment:Alignment.center, fit:BoxFit.contain, animation:"enter"),
+                child: FlareActor("assets/recycle.flr",
+                    alignment: Alignment.center,
+                    fit: BoxFit.contain,
+                    animation: "enter"),
               ),
               SizedBox(
                 height: 30.0,
               ),
               Text(
                 'Join JunkJar',
-                style: GoogleFonts.sriracha(textStyle: TextStyle(color: MyColors.white, fontSize: 50.0,)),
+                style: GoogleFonts.sriracha(
+                    textStyle: TextStyle(
+                  color: MyColors.white,
+                  fontSize: 50.0,
+                )),
               ),
               Text(
                 'Join JunkJar to convert your plastic bottles into \n points to claim exciting gift cards and vouchers!',
-                style: GoogleFonts.sriracha(textStyle: TextStyle(color: MyColors.white, fontSize: 15.0,)),
+                style: GoogleFonts.sriracha(
+                    textStyle: TextStyle(
+                  color: MyColors.white,
+                  fontSize: 15.0,
+                )),
               ),
               SizedBox(
-                height: 100.0,
+                height: 80.0,
               ),
-              GoogleButton(),
+              SignInButton(Buttons.Google, onPressed: (){
+                signInWithGoogle().whenComplete(() {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return FirstScreen();
+                      },
+                    ),
+                  );
+                });
+              },),
+//              GestureDetector(
+//                child: GoogleButton(),
+//                onTap: () {
+//                  print('CLICKeD');
+//                  signInWithGoogle().whenComplete(() {
+//                    Navigator.of(context).push(
+//                      MaterialPageRoute(
+//                        builder: (context) {
+//                          return FirstScreen();
+//                        },
+//                      ),
+//                    );
+//                  });
+//                },
+//              ),
             ],
           ),
         ],
